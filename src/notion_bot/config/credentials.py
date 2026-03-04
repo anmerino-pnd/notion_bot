30451bea55d0e18693d3c9dd33024b04c06e6fdc
Angel Merino <acedeno00@gmail.com>
2026-03-04 12:09:41 -0700

    credentials and config

--- src/notion_bot/config/credentials.py
+++ src/notion_bot/config/credentials.py
@@ -4,4 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ollama_api_key : str = os.getenv('OLLAMA_API_KEY')
-
+notion_integration_token: str = os.getenv('NOTION_INTEGRATION_TOKEN')
+notion_page_token: str = os.getenv('NOTION_PAGE_TOKEN')
+notion_db_key: str = os.getenv('NOTION_DB_KEY')
